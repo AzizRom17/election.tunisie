51a1bd9c3efa53f93fa0c18362eb5f5f15b97d6b
AzizRom17 <aziz.romdhani@gmail.com>
2020-11-11 12:01:40 +0100

    Initial commit

---
+++ src/main/java/tn/isg/projet/aos/election/tunisie/ElectionTunisieApplication.java
@@ -0,0 +1,13 @@
+package tn.isg.projet.aos.election.tunisie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectionTunisieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectionTunisieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tn/isg/projet/aos/election/tunisie/ElectionTunisieApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.isg.projet.aos.election.tunisie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectionTunisieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
